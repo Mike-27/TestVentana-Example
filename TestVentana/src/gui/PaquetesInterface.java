d541684d6efbfa4237dd721f810e07ac878c6a20
rockoland <hymura27@gmail.com>
2023-09-03 06:42:43 -0600

    Agregando tamaño a la ventana por default

--- TestVentana/src/gui/PaquetesInterface.java
+++ TestVentana/src/gui/PaquetesInterface.java
@@ -16,6 +16,7 @@ public class PaquetesInterface {
 	
 	
 	public void contruccion(){
+		ventana.setSize(400,500);
 		ventana.setLayout(gridLayout);
 		ventana.add(panel);
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
